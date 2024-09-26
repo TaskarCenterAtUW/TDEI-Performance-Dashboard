66d7396a0769087e768bbd7a0e8c1dc81727928d
shweta2101 <shwethap@vindago.in>
2024-09-26 16:26:35 +0530

    added tool tips

--- tdei-dashboard/src/components/SystemUsageCards.js
+++ tdei-dashboard/src/components/SystemUsageCards.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Box, Card, Typography } from '@mui/material';
+import { Grid, Box, Card, Typography, Tooltip } from '@mui/material';
 import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
 import DashboardCard from './DashboardCard';
 import GroupIcon from '@mui/icons-material/Group';
@@ -35,111 +35,143 @@ export default function SystemUsageCards({ details }) {
         <Grid item xs={8}>
           <Grid container spacing={2}>
             <Grid item xs={4}>
-              <DashboardCard
-                title={'Users'}
-                value={systemMetrics.totalUsers ?? "0"}
-                icon={<GroupIcon />}
-                gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
-                color={'#fff'}
-              />
+              <Tooltip title="Total number of users in the system" disableInteractive>
+                <Box>
+                  <DashboardCard
+                    title={'Users'}
+                    value={systemMetrics.totalUsers ?? "0"}
+                    icon={<GroupIcon />}
+                    gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
+                    color={'#fff'}
+                  />
+                </Box>
+              </Tooltip>
             </Grid>
             <Grid item xs={4}>
-              <DashboardCard
-                title={'Project Groups'}
-                value={systemMetrics.totalProjectGroups ?? "0"}
-                icon={<Diversity2Icon />}
-                gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
-                color={'#fff'}
-              />
+              <Tooltip title="Total number of project groups" disableInteractive>
+                <Box>
+                  <DashboardCard
+                    title={'Project Groups'}
+                    value={systemMetrics.totalProjectGroups ?? "0"}
+                    icon={<Diversity2Icon />}
+                    gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
+                    color={'#fff'}
+                  />
+                </Box>
+              </Tooltip>
             </Grid>
             <Grid item xs={4}>
-              <DashboardCard
-                title={'Services'}
-                value={systemMetrics.totalServices ?? "0"}
-                icon={<MiscellaneousServicesIcon />}
-                gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
-                color={'#fff'}
-              />
-            </Grid>
-            {/* Dataset Metrics Section */}
-            <Grid item xs={6} sx={{ marginTop: '20px' }}>
-              <CustomTwoValuesCard
-                title={'Dataset Uploads'}
-                subtitle1={'Uploads'}
-                value1={datasetMetrics.totalUploads?.count ?? '0'}
-                subtitle2={'Size Uploaded'}
-                value2={totalSizeUploadedGB.toFixed(2) ?? '0'}  
-                icon={<CloudUploadIcon fontSize="large" sx={{ color: '#8ec5fc' }} />}
-              />
+              <Tooltip title="Total number of services available" disableInteractive>
+                <Box>
+                  <DashboardCard
+                    title={'Services'}
+                    value={systemMetrics.totalServices ?? "0"}
+                    icon={<MiscellaneousServicesIcon />}
+                    gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
+                    color={'#fff'}
+                  />
+                </Box>
+              </Tooltip>
             </Grid>
             <Grid item xs={6} sx={{ marginTop: '20px' }}>
-              <CustomTwoValuesCard
-                title={'Dataset Downloads'}
-                subtitle1={'Downloads'}
-                value1={datasetMetrics.totalDownloads?.count ?? '0'}
-                subtitle2={'Size Downloaded'}
-                value2={totalSizeDownloadedGB.toFixed(2) ?? '0'} 
-                icon={<CloudDownloadIcon fontSize="large" sx={{ color: '#8ec5fc' }} />}
-              />
+              <Tooltip title="Number of datasets uploaded and total size uploaded" disableInteractive>
+                <Box>
+                  <CustomTwoValuesCard
+                    title={'Dataset Uploads'}
+                    subtitle1={'Uploads'}
+                    value1={datasetMetrics.totalUploads?.count ?? '0'}
+                    subtitle2={'Size Uploaded'}
+                    value2={totalSizeUploadedGB.toFixed(2) ?? '0'}  
+                    icon={<CloudUploadIcon fontSize="large" sx={{ color: '#8ec5fc' }} />}
+                  />
+                </Box>
+              </Tooltip>
+            </Grid>
+            <Grid item xs={6} sx={{ marginTop: '20px' }}>
+              <Tooltip title="Number of datasets downloaded and total size downloaded" disableInteractive>
+                <Box>
+                  <CustomTwoValuesCard
+                    title={'Dataset Downloads'}
+                    subtitle1={'Downloads'}
+                    value1={datasetMetrics.totalDownloads?.count ?? '0'}
+                    subtitle2={'Size Downloaded'}
+                    value2={totalSizeDownloadedGB.toFixed(2) ?? '0'} 
+                    icon={<CloudDownloadIcon fontSize="large" sx={{ color: '#8ec5fc' }} />}
+                  />
+                </Box>
+              </Tooltip>
             </Grid>
           </Grid>
         </Grid>
         <Grid item xs={4}>
-          <Card sx={{
-            height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', background: 'linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.2)',
-            borderRadius: '12px',
-            transition: 'transform 0.3s',
-            '&:hover': {
-              transform: 'scale(1.05)',
-              boxShadow: 10,
-            },
-          }}>
+          <Tooltip title="Distribution of services by type (OSW, Flex, Pathways)" disableInteractive placement='top'>
             <Box>
-              <Typography variant="h6" sx={{
-                marginBottom: '12px',
-                fontWeight: 'bold',
-                color: '#333',
-                display: 'flex', justifyContent: 'center', alignItems: 'center'
+              <Card sx={{
+                height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', background: 'linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.2)',
+                borderRadius: '12px',
+                transition: 'transform 0.3s',
+                '&:hover': {
+                  transform: 'scale(1.05)',
+                  boxShadow: 10,
+                },
               }}>
-                Services By Type
-              </Typography>
-              {servicesData.length > 0 ? (
-                <PieChart
-                  series={[
-                    {
-                      arcLabel: (item) => `${Number(item.value ?? 0).toFixed(2)}%`,
-                      arcLabelMinAngle: 10, 
-                      arcLabelRadius: '80%', 
-                      data: servicesData,
-                      valueFormatter,
-                    },
-                  ]}
-                  tooltip={{ show: false }}
-                  sx={{
-                    [`& .${pieArcLabelClasses.root}`]: {
-                      fontWeight: 'bold',
-                      fill: '#fff',
-                      fontSize:'12px'
-                    },
-                  }}
-                  width={400}
-                  height={200}
-                />
-              ) : (
-                <Typography sx={{ fontWeight: 'bold', color: '#333' }}>
-                  No Data Available
-                </Typography>
-              )}
+                <Box sx={{height:'300px'}}>
+                  <Typography variant="h6" sx={{
+                    marginBottom: '12px',
+                    marginTop: '12px',
+                    fontWeight: 'bold',
+                    color: '#333',
+                    display: 'flex', justifyContent: 'center', alignItems: 'center'
+                  }}>
+                    Services By Type
+                  </Typography>
+                  {servicesData.length > 0 ? (
+                    <PieChart
+                      series={[
+                        {
+                          arcLabel: (item) => `${Number(item.value ?? 0).toFixed(2)}%`,
+                          arcLabelMinAngle: 10, 
+                          arcLabelRadius: '80%', 
+                          data: servicesData,
+                          valueFormatter,
+                        },
+                      ]}
+                      tooltip={{ show: false }}
+                      sx={{
+                        [`& .${pieArcLabelClasses.root}`]: {
+                          fontWeight: 'bold',
+                          fill: '#fff',
+                          fontSize:'12px'
+                        },
+                      }}
+                      width={400}
+                      height={210}
+                    />
+                  ) : (
+                    <Typography sx={{ fontWeight: 'bold', color: '#333' }}>
+                      No Data Available
+                    </Typography>
+                  )}
+                </Box>
+              </Card>
             </Box>
-          </Card>
+          </Tooltip>
         </Grid>
         <Grid item xs={6} sx={{ marginTop: '10px' }}>
-          <Card sx={{ boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.1)', height: '375px' }}>
-            <CustomLineChart data={datasetMetrics.downloadsPerMonth ?? {}} />
-          </Card>
+          <Tooltip title="Dataset download statistics over time" disableInteractive>
+            <Box>
+              <Card sx={{ boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.1)', height: '375px' }}>
+                <CustomLineChart data={datasetMetrics.downloadsPerMonth ?? {}} />
+              </Card>
+            </Box>
+          </Tooltip>
         </Grid>
         <Grid item xs={6} sx={{ marginTop: '10px' }}>
-          <ApiMetricsCard apiMetrics={apiMetrics ?? {}} />
+          <Tooltip title="API metrics, including total API calls and calls per endpoint" disableInteractive>
+            <Box>
+              <ApiMetricsCard apiMetrics={apiMetrics ?? {}} />
+            </Box>
+          </Tooltip>
         </Grid>
       </Grid>
     </Box>
--- tdei-dashboard/src/pages/TDEIMetrics/AppMetricsCards.js
+++ tdei-dashboard/src/pages/TDEIMetrics/AppMetricsCards.js
@@ -1,81 +1,70 @@
 import React from 'react';
-import { Grid, Box } from '@mui/material';
+import { Grid, Box, Tooltip } from '@mui/material';
 import DashboardCard from '../../components/DashboardCard';
-import CardTravelIcon from '@mui/icons-material/CardTravel';
-import SettingsIcon from '@mui/icons-material/Settings';
-import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
 import AirportShuttleIcon from '@mui/icons-material/AirportShuttle';
+import SettingsIcon from '@mui/icons-material/Settings';
 import NoCrashIcon from '@mui/icons-material/NoCrash';
+import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
 
 const AppMetricsCards = ({ tdeiAppMetrics }) => {
     const attemptedTrips = tdeiAppMetrics?.accessMapMetrics?.tripResults?.attempted;
     const completedTrips = tdeiAppMetrics?.accessMapMetrics?.tripResults?.completed;
     const tripRatio = attemptedTrips && completedTrips ? (attemptedTrips / completedTrips).toFixed(2) : null;
+
     return (
         <Box sx={{ width: '100%', height: 'auto', padding: '10px' }}>
             <Grid container spacing={2}>
-
-                <Grid
-                    item
-                    xs={12}
-                    sm={6}
-                    md={4}
-                    lg={3}
-                >
-                    <DashboardCard
-                        title={'Annual Trips'}
-                        value={tdeiAppMetrics.accessMapMetrics.annualTrips}
-                        icon={<AirportShuttleIcon />}
-                        gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
-                        color={'#fff'}
-                    />
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Tooltip title="Total number of annual trips recorded." arrow placement="top">
+                        <Box>
+                            <DashboardCard
+                                title={'Annual Trips'}
+                                value={tdeiAppMetrics.accessMapMetrics.annualTrips}
+                                icon={<AirportShuttleIcon />}
+                                gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
+                                color={'#fff'}
+                            />
+                        </Box>
+                    </Tooltip>
                 </Grid>
-                <Grid
-                    item
-                    xs={12}
-                    sm={6}
-                    md={4}
-                    lg={3}
-                >
-                    <DashboardCard
-                        title={'Unique Configurations'}
-                        value={tdeiAppMetrics.accessMapMetrics.uniqueConfigs}
-                        icon={<SettingsIcon />}
-                        gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
-                        color={'#fff'}
-                    />
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Tooltip title="Number of unique configurations used." arrow placement="top">
+                        <Box>
+                            <DashboardCard
+                                title={'Unique Configurations'}
+                                value={tdeiAppMetrics.accessMapMetrics.uniqueConfigs}
+                                icon={<SettingsIcon />}
+                                gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
+                                color={'#fff'}
+                            />
+                        </Box>
+                    </Tooltip>
                 </Grid>
-                <Grid
-                    item
-                    xs={12}
-                    sm={6}
-                    md={4}
-                    lg={3}
-                >
-                    <DashboardCard
-                        title={'Trips Complete Ratio'}
-                        value={tripRatio}
-                        icon={<NoCrashIcon />}
-                        gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
-                        color={'#fff'}
-                    />
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Tooltip title="Ratio of attempted to completed trips." arrow placement="top">
+                        <Box>
+                            <DashboardCard
+                                title={'Trips Complete Ratio'}
+                                value={tripRatio}
+                                icon={<NoCrashIcon />}
+                                gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
+                                color={'#fff'}
+                            />
+                        </Box>
+                    </Tooltip>
                 </Grid>
-                <Grid
-                    item
-                    xs={12}
-                    sm={6}
-                    md={4}
-                    lg={3}
-                >
-                    <DashboardCard
-                        title={'Paying Tenants'}
-                        value={tdeiAppMetrics.payingTenants}
-                        icon={<PermContactCalendarIcon />}
-                        gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
-                        color={'#fff'}
-                    />
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Tooltip title="Number of paying tenants using the platform." arrow placement="top">
+                        <Box>
+                            <DashboardCard
+                                title={'Paying Tenants'}
+                                value={tdeiAppMetrics.payingTenants}
+                                icon={<PermContactCalendarIcon />}
+                                gradient={'linear-gradient(135deg, #4C2880 0%, #8749F2 100%)'}
+                                color={'#fff'}
+                            />
+                        </Box>
+                    </Tooltip>
                 </Grid>
 
             </Grid>
