b2c8c316f38d10018f37b7e5556bb2f6a0876011
shweta2101 <shwethap@vindago.in>
2025-02-20 14:19:30 +0530

    integrated demo regions api call

--- tdei-dashboard/src/utils/contants.js
+++ tdei-dashboard/src/utils/contants.js
@@ -1,3 +1,4 @@
 export const PKI = process.env.REACT_APP_PKI  
 export const ENCRYPTED_KEY = process.env.REACT_APP_ENCRYPTED_KEY
 export const BASE_URL = process.env.REACT_APP_BASE_URL
+export const PROJECT_GROUP = process.env.REACT_APP_PROJECT_GROUP
