a706859a0f782cfce0a8de0df5c1b455c42256d3
Faiz <faiz@Faizs-MacBook-Pro.local>
2025-06-26 11:17:05 +0530

    Dashboard responsive UI

--- tdei-dashboard/src/App.js
+++ tdei-dashboard/src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
-import { AppBar, Tabs, Tab, Toolbar, Typography, Box, Container } from "@mui/material";
+import { AppBar, Tabs, Tab, Toolbar, Typography, Box, Container, ListItem, ListItemButton, ListItemText, Divider, List, Drawer, IconButton, Button } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import SystemUsageMetrics from "./pages/SystemUsageMetrics/SystemUsageMetrics";
 import DataMetrics from "./pages/DataMetrics/DataMetrics";
@@ -10,6 +11,7 @@ import './App.css';
 
 function App() {
   const [activeTab, setActiveTab] = useState(0);
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const systemUsageRef = useRef(null);
   const dataMetricsRef = useRef(null);
@@ -23,58 +25,124 @@ function App() {
     });
   };
 
+  const drawerWidth = 250;
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'left' }}>
+      <Typography sx={{ fontSize: '1rem', fontWeight: 'bold', m: 2 }}>
+        TDEI Performance Dashboard
+      </Typography>
+      <Divider />
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: 'left' }}>
+              <ListItemText onClick={() => handleScroll(systemUsageRef, 0)}>System usage metrics</ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: 'left' }}>
+              <ListItemText onClick={() => handleScroll(dataMetricsRef, 1)}>Data metrics</ListItemText>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: 'left' }}>
+              <ListItemText onClick={() => handleScroll(tdeiMetricsRef, 2)}>TDEI metrics</ListItemText>
+            </ListItemButton>
+          </ListItem>
+      </List>
+    </Box>
+  );
+
   return (
     <ThemeProvider theme={theme}>
       <AppBar position="fixed">
         <Toolbar>
-        <img src={tdeiLogo} alt="TDEI Logo" className="logoImage" />
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          <img src={tdeiLogo} alt="TDEI Logo" className="logoImage" />
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' }, marginLeft: 'auto' }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            // variant="h6"
+            component="div"
+            sx={{ fontSize: { xs: '1rem', sm: '1rem', md: '1.2rem',}, flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
             TDEI Performance Dashboard
           </Typography>
-          <Tabs
-            value={activeTab}
-            aria-label="performance dashboard tabs"
-            textColor="white"
-            sx={{
-              '& .MuiTabs-indicator': {
-                display: 'none',
-              },
-            }}
-          >
-            <Tab
-              label="System usage metrics"
-              onClick={() => handleScroll(systemUsageRef, 0)}
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Tabs
+              value={activeTab}
+              aria-label="performance dashboard tabs"
+              textColor="white"
               sx={{
-                color: 'white',
-                fontWeight: activeTab === 0 ? 'bold' : 'normal',
-                fontSize: activeTab === 0 ? '1.1rem' : '1rem',
-                textTransform: 'none',
+                '& .MuiTabs-indicator': {
+                  display: 'none',
+                },
               }}
-            />
-            <Tab
-              label="Data metrics"
-              onClick={() => handleScroll(dataMetricsRef, 1)}
-              sx={{
-                color: 'white',
-                fontWeight: activeTab === 1 ? 'bold' : 'normal',
-                fontSize: activeTab === 1 ? '1.1rem' : '1rem',
-                textTransform: 'none',
-              }}
-            />
-            <Tab
-              label="TDEI metrics"
-              onClick={() => handleScroll(tdeiMetricsRef, 2)}
-              sx={{
-                color: 'white',
-                fontWeight: activeTab === 2 ? 'bold' : 'normal',
-                fontSize: activeTab === 2 ? '1.1rem' : '1rem',
-                textTransform: 'none',
-              }}
-            />
-          </Tabs>
+            >
+              <Tab
+                label="System usage metrics"
+                onClick={() => handleScroll(systemUsageRef, 0)}
+                sx={{
+                  color: 'white',
+                  fontWeight: activeTab === 0 ? 'bold' : 'normal',
+                  fontSize: activeTab === 0 ? '1.1rem' : '1rem',
+                  textTransform: 'none',
+                }}
+              />
+              <Tab
+                label="Data metrics"
+                onClick={() => handleScroll(dataMetricsRef, 1)}
+                sx={{
+                  color: 'white',
+                  fontWeight: activeTab === 1 ? 'bold' : 'normal',
+                  fontSize: activeTab === 1 ? '1.1rem' : '1rem',
+                  textTransform: 'none',
+                }}
+              />
+              <Tab
+                label="TDEI metrics"
+                onClick={() => handleScroll(tdeiMetricsRef, 2)}
+                sx={{
+                  color: 'white',
+                  fontWeight: activeTab === 2 ? 'bold' : 'normal',
+                  fontSize: activeTab === 2 ? '1.1rem' : '1rem',
+                  textTransform: 'none',
+                }}
+              />
+            </Tabs>
+          </Box>
         </Toolbar>
       </AppBar>
-      <Box sx={{ marginTop: "100px", marginRight:'30px',marginLeft:'20px' }}> 
+
+      {/* Mobile nab bar */}
+      <nav>
+        <Drawer
+          // container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      
+      <Box className="content__block">
         <Box ref={systemUsageRef}>
           <SystemUsageMetrics />
         </Box>
--- tdei-dashboard/src/pages/TDEIMetrics/TDEIMetrics.js
+++ tdei-dashboard/src/pages/TDEIMetrics/TDEIMetrics.js
@@ -10,14 +10,14 @@ const TDEIMetrics = () => {
   const { accessMapMetrics, routeMetrics } = tdeiAppMetrics || {};
 
   return (
-    <Box sx={{ width: '96%', height: '100vh', paddingLeft: '16px', padding: '16px' }}>
-      <Typography variant="h4" sx={{ fontWeight: 'bold', marginBottom: '8px', color: '#333' }}>
+    <Box className="data__container">
+      <div className='data__title'>
         TDEI Metrics
-      </Typography>
-      <Box sx={{ paddingTop: '30px' }}>
+      </div>
+      <Box>
         <AppMetricsCards tdeiAppMetrics={tdeiAppMetrics} />
-        <Grid container spacing={2} sx={{ paddingTop: '30px' }}>
-          <Grid item xs={12} sm={6}>
+        <Grid container spacing={2} sx={{ marginTop: '40px' }}>
+          <Grid item xs={12} sm={12} md={6}>
             <Tooltip title="Satisfaction rates categorized by user feedback." arrow>
               <Card sx={{ padding: '16px', textAlign: 'center', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.2)', height: '370px' }}>
                 <Box sx={{ width: '100%', textAlign: 'center', marginBottom: '20px', height: '300px' }}>
@@ -29,7 +29,7 @@ const TDEIMetrics = () => {
               </Card>
             </Tooltip>
           </Grid>
-          <Grid item xs={12} sm={3}>
+          <Grid item xs={12} sm={6} md={3}>
             <Tooltip title="Percent of Origin Destination pairs successfully routed for unconstrained pedestrians.  Each pair is an apartment building in covered area and nearest critical service within half mile." arrow>
               <Card
                 sx={{
@@ -56,7 +56,7 @@ const TDEIMetrics = () => {
               </Card>
             </Tooltip>
           </Grid>
-          <Grid item xs={12} sm={3}>
+          <Grid item xs={12} sm={6} md={3}>
             <Tooltip title="Percent of Origin Destination pairs successfully routed for wheelchair users.  Each pair is an apartment building in covered area and nearest critical service within half mile." arrow >
               <Card
                 sx={{
